aa04a26b59fe0efb6a771fb821ec2adb12696796
MXerFix <maksim2003003@gmail.com>
2024-02-27 02:52:54 +0300

    docker fix

--- df_designer_front/vite.config.ts
+++ df_designer_front/vite.config.ts
@@ -1,34 +1,49 @@
-import { UserConfig, defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
-const apiRoutes = ["/flows" , "/flows", "/health", '/build', '/socket', '/builds', '/run', '/runs', '/bot', '/bot/build/start', '/bot/build/status', '/bot/build/stop', '/bot/run/start', '/bot/run/status', '/bot/run/stop'];
+import { UserConfig, defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
+const apiRoutes = [
+  '/flows',
+  '/health',
+  '/build',
+  '/socket',
+  '/builds',
+  '/run',
+  '/runs',
+  '/bot',
+  '/bot/build/start',
+  '/bot/build/status/',
+  '/bot/build/stop',
+  '/bot/run/start',
+  '/bot/run/status',
+  '/bot/run/stop',
+]
 
 // Use environment variable to determine the target.
-const target = process.env.VITE_PROXY_TARGET || "http://0.0.0.0:8000";
+const target = process.env.VITE_PROXY_TARGET || 'http://0.0.0.0:8000'
 
 console.log(process.env.VITE_PROXY_TARGET)
 
 const proxyTargets = apiRoutes.reduce((proxyObj, route) => {
   proxyObj[route] = {
     target: target,
-    changeOrigin: true,
+    changeOrigin: false,
     secure: false,
     ws: true,
-  };
-  return proxyObj;
-}, {});
+  }
+  return proxyObj
+}, {})
 export default defineConfig((): UserConfig => {
   return {
     build: {
-      outDir: "../df_designer/static",
+      outDir: '../df_designer/static',
     },
     plugins: [react(), svgr()],
     server: {
-      host: "0.0.0.0",
+      host: '0.0.0.0',
       port: 3000,
       proxy: {
         ...proxyTargets,
       },
     },
-  };
-});
+  }
+})
