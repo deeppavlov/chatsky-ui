5842942063b965ab12081fbe7034ceabd6689077
Ramimashkouk <ramima1996@gmail.com>
2024-03-28 15:51:36 +0300

    feat: Provide logging system

--- backend/df_designer/app/__init__.py
+++ backend/df_designer/app/__init__.py
@@ -1,14 +1,23 @@
 import aiofiles
 import asyncio
 from datetime import datetime
+import logging
+from pathlib import Path
 import time
 from typing import List
 from omegaconf import OmegaConf
 
-from app.core.logger_config import get_logger
+from app.core.logger_config import get_logger, setup_logging
 from app.core.config import settings
 
-logger = get_logger(__name__)
+
+def _map_to_str(params: dict):
+    for k, v in params.items():
+        if isinstance(v, datetime):
+            params[k] = v.strftime("%Y-%m-%dT%H:%M:%S")
+        elif isinstance(v, Path):
+            params[k] = str(v)
+
 
 class Process:
     def __init__(self, id_: int, preset_end_status = ""):
@@ -16,11 +25,12 @@ class Process:
         self.preset_end_status: str = preset_end_status
         self.status: str = "null"
         self.timestamp: datetime = datetime.now()
-        self.log_path: str = "./logs/"
-        self.process: asyncio.subprocess.Process = None # pylint: disable=no-member #TODO: is naming ok? 
+        self.log_path: Path
+        self.process: asyncio.subprocess.Process # pylint: disable=no-member #TODO: is naming ok? 
+        self.logger: logging.Logger
 
     async def start(self, cmd_to_run):
-        async with aiofiles.open("process_io.log", "a", encoding="UTF-8") as file: #TODO: log to files
+        async with aiofiles.open(self.log_path, "a", encoding="UTF-8") as file: #TODO: log to files
             self.process = await asyncio.create_subprocess_exec(
                 *cmd_to_run.split(),
                 stdout=asyncio.subprocess.PIPE,
@@ -31,7 +41,7 @@ class Process:
     def get_full_info(self) -> dict:
         self.check_status()
         return {
-            key: getattr(self, key) for key in self.__dict__ if key != "process"
+            key: getattr(self, key) for key in self.__dict__ if key not in ["process", "logger"]
         }
 
     def set_full_info(self, params_dict):
@@ -44,7 +54,7 @@ class Process:
     def periodically_check_status(self):
         while True:
             self.update_db_info() # check status and update db
-            logger.info("Status of process '%s': %s",self.id, self.status)
+            self.logger.info("Status of process '%s': %s",self.id, self.status)
             if self.status in ["stopped", "completed", "failed"]:
                 break
             time.sleep(2) #TODO: ?sleep time shouldn't be constant
@@ -70,7 +80,7 @@ class Process:
         elif self.process.returncode == -15:
             self.status = "stopped"
         else:
-            logger.warning(
+            self.logger.warning(
                 "Unexpected code was returned: '%s'. A non-zero return code indicates an error.",
                 self.process.returncode
             )
@@ -81,7 +91,7 @@ class Process:
         if self.process is None:  # Check if a process has been started
             raise RuntimeError(f"Cannot stop a process '{self.id}' that has not started yet.")
         try:
-            logger.debug("Terminating process '%s'", self.id)
+            self.logger.debug("Terminating process '%s'", self.id)
             self.process.terminate()
             await self.process.wait()
         except ProcessLookupError as exception:
@@ -104,11 +114,17 @@ class RunProcess(Process):
         super().__init__(id_, preset_end_status)
         self.build_id: int = build_id
 
+        log_name: str = "_".join([str(id_), datetime.now().time().strftime("%H%M%S")])
+        self.log_path: Path = setup_logging("runs", log_name)
+        self.logger = get_logger(str(id_), self.log_path)
+
     def update_db_info(self):
         # save current run info into RUNS_PATH
         runs_conf = settings.read_conf(settings.RUNS_PATH)
+
         run_params = self.get_full_info()
-        run_params["timestamp"] = run_params["timestamp"].strftime("%Y-%m-%dT%H:%M:%S")
+        _map_to_str(run_params)
+
         for run in runs_conf:
             if run.id == run_params["id"]:
                 for key, value in run_params.items():
@@ -139,12 +155,16 @@ class BuildProcess(Process):
         super().__init__(id_, preset_end_status)
         self.runs: List[int] = []
 
+        log_name: str = "_".join([str(id_), datetime.now().time().strftime("%H%M%S")])
+        self.log_path: Path = setup_logging("builds", log_name)
+        self.logger = get_logger(str(id_), self.log_path)
+
     def update_db_info(self):
         # save current build info into BUILDS_PATH
         builds_conf = settings.read_conf(settings.BUILDS_PATH)
 
         build_params = self.get_full_info()
-        build_params["timestamp"] = build_params["timestamp"].strftime("%Y-%m-%dT%H:%M:%S")
+        _map_to_str(build_params)
 
         for build in builds_conf:
             if build.id == build_params["id"]:
--- backend/df_designer/app/core/logger_config.py
+++ backend/df_designer/app/core/logger_config.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 import logging
+from pathlib import Path
+import os
 
 from app.core.config import settings
 
@@ -10,14 +13,28 @@ LOG_LEVELS: dict[str, int] = {
     "debug": logging.DEBUG,
 }
 
+def setup_logging(log_type: str, log_name: str) -> Path:
+    # Ensure log_type is either 'builds' or 'runs'
+    if log_type not in ['builds', 'runs']:
+        raise ValueError("log_type must be 'builds' or 'runs'")
 
-def get_logger(name):
+    today_date = datetime.now().strftime("%Y%m%d")
+    log_directory = settings.DIR_LOGS / log_type / today_date
+
+    os.makedirs(log_directory, exist_ok=True)
+
+    log_file = log_directory / f"{log_name}.log"
+    if not os.path.exists(log_file):
+        open(log_file, 'w', encoding="UTF-8").close()
+    return log_file
+
+def get_logger(name, file_handler_path: Path = settings.DIR_LOGS/ "logs.log"):
     logger = logging.getLogger(name)
     logger.propagate = False
     logger.setLevel(LOG_LEVELS[settings.LOG_LEVEL])
 
     c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler(settings.DIR_LOGS)
+    f_handler = logging.FileHandler(file_handler_path)
     c_handler.setLevel(LOG_LEVELS[settings.LOG_LEVEL])
     f_handler.setLevel(LOG_LEVELS[settings.LOG_LEVEL])
 
