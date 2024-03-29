514997eadcf1edef3d9ae0cdc7aabd6fb267c47c
Ramimashkouk <ramima1996@gmail.com>
2024-03-29 17:11:44 +0300

    feat: Serve static files

--- backend/df_designer/app/main.py
+++ backend/df_designer/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from app.api.api_v1.api import api_router
+from app.core.config import settings
 
 app = FastAPI(title="DF Designer")
 
@@ -12,6 +14,12 @@ app.add_middleware(
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
+)
+
+app.mount(
+    "/static",
+    StaticFiles(directory=settings.static_files),
+    name="static",
 )
 
 root_router = APIRouter()
