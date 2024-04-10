0b4b55298a1496957ca4cd2f6e2a114109d1fb04
Ramimashkouk <ramima1996@gmail.com>
2024-04-10 11:56:50 +0300

    feat: Include versioning in endpoint paths

--- backend/df_designer/app/api/api_v1/api.py
+++ backend/df_designer/app/api/api_v1/api.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.endpoints import bot, flows
-
+from app.core.config import settings
 
 api_router = APIRouter()
 
-api_router.include_router(flows.router, prefix="/flows", tags=["flows"])
-api_router.include_router(bot.router, prefix="/bot", tags=["bot"])
+api_router.include_router(flows.router, prefix="/".join([settings.API_V1_STR, "flows"]), tags=["flows"])
+api_router.include_router(bot.router, prefix="/".join([settings.API_V1_STR, "bot"]), tags=["bot"])
