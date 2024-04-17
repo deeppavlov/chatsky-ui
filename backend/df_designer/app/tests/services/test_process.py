5a475b5b68f7895959767cc5aaccc91e86bb3554
Ramimashkouk <ramima1996@gmail.com>
2024-04-17 18:13:04 +0300

    test: Add unit tests for services

---
+++ backend/df_designer/app/tests/services/test_process.py
@@ -0,0 +1,59 @@
+import asyncio
+from app.services.process import Process, RunProcess
+from pathlib import Path
+import pytest
+import logging
+
+
+from app.core.logger_config import get_logger
+logger = get_logger(__name__)
+class TestRunProcess:
+    @pytest.fixture(scope="session")
+    def run_process(self):
+        async def _run_process(cmd_to_run):
+            process = RunProcess(id_=0)
+            await process.start(cmd_to_run)
+            return process
+
+        return _run_process
+
+    # def test_update_db_info(self, run_process):
+    #     process = await run_process("echo 'Hello df_designer'")
+    #     process.update_db_info()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("cmd_to_run, status", [
+        ("sleep 10000", "running"),
+        ("bash -c 'exit 1'", "failed"),
+        ("echo 'Hello df_designer'", "completed"),
+    ])
+    async def test_check_status(self, run_process, cmd_to_run, status):
+        process = await run_process(cmd_to_run)
+        await asyncio.sleep(2)
+        assert process.check_status() == status
+
+    # def test_periodically_check_status(self, run_process):
+    #     process = await run_process("sleep 10000")
+    #     run_process.periodically_check_status()
+
+    @pytest.mark.asyncio
+    async def test_stop(self, run_process):
+        process = await run_process("sleep 10000")
+        await process.stop()
+        assert process.process.returncode == -15
+
+    @pytest.mark.asyncio
+    async def test_read_stdout(self, run_process):
+        process = await run_process("echo Hello df_designer")
+        output = await process.read_stdout()
+        assert output.strip().decode() == "Hello df_designer"
+
+    @pytest.mark.asyncio
+    async def test_write_stdout(self, run_process):
+        process = await run_process("cat")
+        process.write_stdin(b"DF_Designer team welcome you.\n")
+        output = await process.process.stdout.readline()
+        assert output.decode().strip() == "DF_Designer team welcome you."
+
+class TestBuildProcess:
+    pass
---
+++ backend/df_designer/app/tests/services/test_process_manager.py
@@ -0,0 +1,61 @@
+import asyncio
+import pytest
+from random import randint
+from typing import List
+
+from app.services.process_manager import RunManager
+from app.schemas.preset import Preset
+from app.core.logger_config import get_logger
+
+
+logger = get_logger(__name__)
+
+class TestRunManager:
+    @pytest.fixture(scope="session")
+    def preset(self):
+        return Preset(
+            wait_time=0,
+            end_status="loop",
+        )
+
+    @pytest.fixture(scope="session")
+    def run_manager(self, preset):
+        async def _run_manager():
+            manager = RunManager()
+            await manager.start(build_id=0, preset=preset)
+            return manager
+        return _run_manager
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("status", ["success", "with_error"])
+    async def test_stop(self, run_manager, status):
+        manager = await run_manager()
+        if status == "success":
+            process_id = manager.get_last_id()
+            await manager.stop(process_id)
+            assert manager.processes[process_id].process.returncode == -15
+        else:
+            process_id = randint(1000, 2000)
+            with pytest.raises((RuntimeError, ProcessLookupError)):
+                await manager.stop(process_id)
+
+    # def test_check_status(self, run_manager, preset):
+        # pass
+
+    @pytest.mark.asyncio
+    async def test_get_process_info(self, run_manager):
+        manager = await run_manager()
+        run_info = await manager.get_run_info(22) #TODO: change 22 to default id in env var 
+        assert isinstance(run_info, dict) and "id" in run_info
+
+    @pytest.mark.asyncio
+    async def test_get_full_info(self, run_manager):
+        manager = await run_manager()
+        full_info = await manager.get_full_info(0, 10)
+        assert isinstance(full_info, list) and  isinstance(full_info[0], dict) and len(full_info) == 10
+
+    @pytest.mark.asyncio
+    async def test_fetch_run_logs(self, run_manager):
+        manager = await run_manager()
+        logs = await manager.fetch_run_logs(22, 0, 10) #TODO: change 22 to default id in env var
+        assert isinstance(logs, list)
