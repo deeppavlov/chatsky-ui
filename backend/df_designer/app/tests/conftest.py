c0643b67612e5df8027499de076284e540081643
Ramimashkouk <ramima1996@gmail.com>
2024-04-26 17:55:52 +0300

    test: Add backend e2e

--- backend/df_designer/app/tests/conftest.py
+++ backend/df_designer/app/tests/conftest.py
@@ -1,7 +1,11 @@
+from contextlib import asynccontextmanager
 from typing import Generator
 
+import httpx
 import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
+from httpx_ws.transport import ASGIWebSocketTransport
 
 from app.main import app
 from app.schemas.pagination import Pagination
@@ -9,6 +13,27 @@ from app.schemas.preset import Preset
 from app.services.process import RunProcess
 from app.services.process_manager import BuildManager, RunManager
 from app.services.websocket_manager import WebSocketManager
+
+
+async def start_process(async_client: AsyncClient, endpoint, preset_end_status) -> httpx.Response:
+    return await async_client.post(
+        endpoint,
+        json={"wait_time": 0.1, "end_status": preset_end_status},
+    )
+
+
+@asynccontextmanager
+async def override_dependency(mocker_obj, get_manager_func):
+    process_manager = get_manager_func()
+    process_manager.check_status = mocker_obj.AsyncMock()
+    app.dependency_overrides[get_manager_func] = lambda: process_manager
+    try:
+        yield process_manager
+    finally:
+        for _, process in process_manager.processes.items():
+            if process.process.returncode is None:
+                await process.stop()
+        app.dependency_overrides = {}
 
 
 @pytest.fixture
@@ -53,3 +78,9 @@ def build_manager():
 @pytest.fixture
 def websocket_manager():
     return WebSocketManager()
+
+
+@pytest.fixture
+async def websocket_client() -> AsyncClient:
+    async with AsyncClient(transport=ASGIWebSocketTransport(app), base_url="http://test") as client:
+        yield client
