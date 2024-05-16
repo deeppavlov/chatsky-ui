78bd34a1b2e53be0715f8dcca4c4c458946310ad
Ramimashkouk <ramima1996@gmail.com>
2024-05-16 23:31:40 +0300

    chore: Check path before passing to cmd

--- backend/df_designer/app/cli.py
+++ backend/df_designer/app/cli.py
@@ -77,6 +77,8 @@ def run_bot(build_id: int, project_dir: str = settings.work_directory, preset: s
 @cli.command("run_scenario")
 def run_scenario(build_id: int, project_dir: str = ".", call_from_open_event_loop: bool = False):
     script_path = Path(project_dir) / "bot" / "scripts" / f"build_{build_id}.yaml"
+    if not script_path.exists():
+        raise FileNotFoundError(f"File {script_path} doesn't exist")
     command_to_run = f"poetry run python {project_dir}/app.py --script-path {script_path}"
     if call_from_open_event_loop:
         loop = asyncio.get_event_loop()
