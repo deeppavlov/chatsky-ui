6eb7c093973ed023a71bbf72ed82582b52dd6d12
Ramimashkouk <ramima1996@gmail.com>
2024-06-24 17:52:03 +0300

    fix: Update the returned dff cnds format

--- backend/df_designer/app/api/api_v1/endpoints/dff_services.py
+++ backend/df_designer/app/api/api_v1/endpoints/dff_services.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from app.api.deps import get_index
 from app.core.logger_config import get_logger
 from app.services.index import Index
-from app.clients.dff_client import get_dff_objects
+from app.clients.dff_client import get_dff_conditions
 
 router = APIRouter()
 
@@ -17,8 +17,6 @@ async def search_service(service_name: str, index: Index = Depends(get_index)):
 
 
 @router.get("/get_conditions", status_code=200)
-async def get_all_services():
-    native_services = get_dff_objects()
-    native_conditions = [k.split(".")[-1] for k, _ in native_services.items() if k.startswith("dff.cnd.")]
-
+async def get_conditions():
+    native_conditions = get_dff_conditions()
     return native_conditions
--- backend/df_designer/app/clients/dff_client.py
+++ backend/df_designer/app/clients/dff_client.py
@@ -1,5 +1,24 @@
 from dff.pipeline.pipeline import script_parsing
+import dff.script.conditions as cnd
 
 
-def get_dff_objects():
-    return script_parsing.get_dff_objects()
+AUTO_COMPLETION_MAP = {
+    "exact_match": "cnd.exact_match(Message(\"hello\"))(ctx, pipeline)",
+    "regexp": "cnd.regexp(r\"how are you\", re.IGNORECASE)(ctx, pipeline)",
+    "any": "cnd.any([hi_lower_case_condition, cnd.exact_match(Message(\"hello\"))])(ctx, pipeline)",
+    "all": "cnd.all([cnd.regexp(r\"talk\"), cnd.regexp(r\"about.*music\")])(ctx, pipeline)",
+}
+
+def get_dff_conditions():
+    native_services = script_parsing.get_dff_objects()
+    native_conditions = [k.split(".")[-1] for k, _ in native_services.items() if k.startswith("dff.cnd.")]
+    cnd_full_info = []
+    for condition in native_conditions:
+        cnd_full_info.append({
+            "label": f"cnd.{condition}",
+            "type": "function",
+            "info": getattr(cnd, condition).__doc__,
+            "apply": AUTO_COMPLETION_MAP.get(condition, "cnd.<DFF_CONDITION>()(ctx, pipeline)"),
+        })
+
+    return cnd_full_info
