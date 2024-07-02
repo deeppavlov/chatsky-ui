dc84d6c3a36a47c4845199c11dae673915103a47
MXerFix <maksim2003003@gmail.com>
2024-07-02 15:40:07 +0300

    update: change lint request type

--- frontend/src/api/services.ts
+++ frontend/src/api/services.ts
@@ -1,10 +1,9 @@
-import { $v1 } from '.';
-
+import { $v1 } from "."
 
 export const get_condition_methods = async () => {
   return (await $v1.get("/services/get_conditions")).data
 }
 
 export const lint_service = async (text: string) => {
-  return (await $v1.post(`/services/lint_snippet?snippet=${text}`)).data
+  return (await $v1.post(`/services/lint_snippet`, { code: text })).data
 }
