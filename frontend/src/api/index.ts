b6a9a920bb48100a0f6184014adcf47df075b817
MXerFix <maksim2003003@gmail.com>
2024-07-02 13:20:54 +0300

    fix: fix url bug

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { DEV, VITE_BASE_API_URL } from "../env.consts";
 
 // const baseURL = VITE_BASE_API_URL ?? "http://localhost:8000/api/v1"
-const baseURL = DEV ? VITE_BASE_API_URL : window.location.href.replace(window.location.pathname, '/api/v1').replace('localhost', '0.0.0.0')
+const baseURL = DEV ? VITE_BASE_API_URL : window.location.href.replace(window.location.pathname, '/api/v1')
 
 export const $v1 = axios.create({
   baseURL: baseURL
