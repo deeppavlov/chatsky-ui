ae584cf970311f3058288da39190bf0fd8e94a77
MXerFix <maksim2003003@gmail.com>
2024-07-26 10:16:24 +0300

    feat: text response improvements

--- frontend/src/modals/ResponseModal/ResponseModal.tsx
+++ frontend/src/modals/ResponseModal/ResponseModal.tsx
@@ -17,22 +17,24 @@ import { flowContext } from "../../contexts/flowContext"
 import { NodeDataType } from "../../types/NodeTypes"
 import { responseType, responseTypeType } from "../../types/ResponseTypes"
 import PythonResponse from "./components/PythonResponse"
+import TextResponse from "./components/TextResponse"
 
-type ResponseModalTab = "Using LLM" | "Python code" | "Custom"
+type ResponseModalTab = "Using LLM" | "Python code" | "Custom" | "Text"
 
 type ResponseModalProps = {
   data: NodeDataType
+  setData: React.Dispatch<React.SetStateAction<NodeDataType>>
   response: responseType
   size?: ModalProps["size"]
   isOpen: boolean
   onClose: () => void
 }
 
-const ResponseModal = ({ isOpen, onClose, data, response, size = "3xl" }: ResponseModalProps) => {
+const ResponseModal = ({ isOpen, onClose, data, setData, response, size = "3xl" }: ResponseModalProps) => {
   const { getNode, setNodes, getNodes } = useReactFlow()
-  const { flows, quietSaveFlows } = useContext(flowContext)
+  const { flows, quietSaveFlows, updateFlow } = useContext(flowContext)
   const { flowId } = useParams()
-  const [selected, setSelected] = useState<responseTypeType>("python")
+  const [selected, setSelected] = useState<responseTypeType>(response.type ?? "python")
   // const [nodeDataState, setNodeDataState] = useState(data)
   const [currentResponse, setCurrentResponse] = useState(response)
   const setSelectedHandler = (key: responseTypeType) => {
@@ -46,16 +48,16 @@ const ResponseModal = ({ isOpen, onClose, data, response, size = "3xl" }: Respon
   }[] = useMemo(
     () => [
       {
-        title: "Using LLM",
-        value: "llm",
-      },
-      {
         title: "Python code",
         value: "python",
       },
       {
-        title: "Custom",
-        value: "custom",
+        title: "Text",
+        value: "text",
+      },
+      {
+        title: "Using LLM",
+        value: "llm",
       },
     ],
     []
@@ -71,7 +73,12 @@ const ResponseModal = ({ isOpen, onClose, data, response, size = "3xl" }: Respon
         />
       ),
       custom: <div>Custom</div>,
-      text: <div>text</div>,
+      text: (
+        <TextResponse
+          response={currentResponse}
+          setData={setCurrentResponse}
+        />
+      ),
     }),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [currentResponse]
@@ -91,6 +98,7 @@ const ResponseModal = ({ isOpen, onClose, data, response, size = "3xl" }: Respon
       }
       const new_nodes = nodes.map((node) => (node.id === data.id ? new_node : node))
       setNodes(() => new_nodes)
+      setData(new_node.data)
       // currentFlow.data.nodes = nodes.map((node) => (node.id === data.id ? new_node : node))
       // updateFlow(currentFlow)
       quietSaveFlows()
