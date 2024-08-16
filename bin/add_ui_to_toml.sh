950256414731f128f4aa49707c42da9794c04055
Ramimashkouk <ramima1996@gmail.com>
2024-08-16 15:45:13 +0800

    fix .sh file

--- bin/add_ui_to_toml.sh
+++ bin/add_ui_to_toml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find the latest version of the wheel file
-VERSION=$(ls ../backend/dist/chatsky_ui-*.whl | grep -oP 'chatsky_ui-\K[^\-]+' | head -n 1)
+VERSION=$(basename $(ls ../backend/dist/chatsky_ui-*.whl) | sed -E 's/chatsky_ui-([^-]+)-.*/\1/' | head -n 1)
 
 # Add the specific version to my project
 poetry add ../backend/dist/chatsky_ui-$VERSION-py3-none-any.whl
