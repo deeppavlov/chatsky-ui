4f9bb79b38fbbcd955b73abe866ebc7df7824917
Ramimashkouk <ramima1996@gmail.com>
2024-09-28 19:42:56 +0800

    install poetry and build dist

--- install_wheel.sh
+++ install_wheel.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Install poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Add poetry to PATH
+export PATH="$HOME/.local/bin:$PATH"
+
+# Build the project to create the dist directory and .whl file
+poetry build
+
 # Find the first .whl file in the dist directory and install it
 WHEEL_FILE=$(ls dist/*.whl | head -n 1)
 if [ -z "$WHEEL_FILE" ]; then
