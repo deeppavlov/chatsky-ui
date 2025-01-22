03022bdd5f9649b04b40b756aa4b4695897755a8
saga6569 <aristov6569@gmail.com>
2025-01-22 13:03:47 +0500

    remove ;

--- frontend/src/modals/SlotsModals/components/SlotItem.tsx
+++ frontend/src/modals/SlotsModals/components/SlotItem.tsx
@@ -1,17 +1,17 @@
-import { Button } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import DefTextarea from "../../../UI/Input/DefTextarea";
-import EditPenIcon from "../../../icons/EditPenIcon";
-import TrashIcon from "../../../icons/TrashIcon";
-import { SlotType } from "../../../types/FlowTypes";
-import DefInput from "../../../UI/Input/DefInput";
+import { Button } from "@nextui-org/react"
+import { useEffect, useState } from "react"
+import DefTextarea from "../../../UI/Input/DefTextarea"
+import EditPenIcon from "../../../icons/EditPenIcon"
+import TrashIcon from "../../../icons/TrashIcon"
+import { SlotType } from "../../../types/FlowTypes"
+import DefInput from "../../../UI/Input/DefInput"
 
 type SlotItemType = {
- slot: SlotType;
- setSlots: (updatedSlot: SlotType) => void;
- onDelete: (slotId: string) => void;
- is_create_modal?: boolean;
-};
+ slot: SlotType
+ setSlots: (updatedSlot: SlotType) => void
+ onDelete: (slotId: string) => void
+ is_create_modal?: boolean
+}
 
 const SlotItem = ({
  slot,
@@ -19,11 +19,11 @@ const SlotItem = ({
  onDelete,
  is_create_modal,
 }: SlotItemType) => {
- const [name, setName] = useState<string>(slot.name ?? "");
- const [type, setType] = useState<"RegexpSlot" | "">(slot.type ?? "");
- const [method, setMethod] = useState<string>(slot.method ?? "");
- const [value, setValue] = useState<string>(slot.value ?? "");
- const [index, setIndex] = useState<number>(0);
+ const [name, setName] = useState<string>(slot.name ?? "")
+ const [type, setType] = useState<"RegexpSlot" | "">(slot.type ?? "")
+ const [method, setMethod] = useState<string>(slot.method ?? "")
+ const [value, setValue] = useState<string>(slot.value ?? "")
+ const [index, setIndex] = useState<number>(0)
 
  useEffect(() => {
   setSlots({
@@ -32,11 +32,11 @@ const SlotItem = ({
    type,
    method,
    value,
-  });
- }, [name, type, method, value]);
+  })
+ }, [name, type, method, value])
 
 
- console.log(value.length);
+ console.log(value.length)
 
  return (
   <div className="my-1">
@@ -93,7 +93,7 @@ const SlotItem = ({
     )}
    </div>
   </div>
- );
-};
+ )
+}
 
-export default SlotItem;
+export default SlotItem
