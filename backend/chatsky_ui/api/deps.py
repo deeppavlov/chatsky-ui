54abaca6c04ccff85473e71a444c4284a2bb555d
ZergLev <vasilev.sv@phystech.edu>
2025-01-30 00:39:41 +0300

    api/deps.py review changes done

--- backend/chatsky_ui/api/deps.py
+++ backend/chatsky_ui/api/deps.py
@@ -4,7 +4,7 @@ build_manager = BuildManager()
 
 
 def get_build_manager() -> BuildManager:
-    """Returns the `build` process manager."""
+    """Returns the only used instance of `build` process manager."""
     build_manager.set_logger()
     build_manager.set_bot_repo_manager()
     build_manager.set_graph_repo_manager()
@@ -15,7 +15,7 @@ run_manager = RunManager()
 
 
 def get_run_manager() -> RunManager:
-    """Returns the `run` process manager."""
+    """Returns the only used instance of `run` process manager."""
     run_manager.set_logger()
     run_manager.set_bot_repo_manager()
     run_manager.set_graph_repo_manager()
