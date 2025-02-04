3608b9ff475023ae8ed5734cb2e61388d5227bca
ZergLev <vasilev.sv@phystech.edu>
2025-02-04 11:45:06 +0300

    service_replacer and consts.py docs drafted, PR ready for review

--- backend/chatsky_ui/services/json_converter/consts.py
+++ backend/chatsky_ui/services/json_converter/consts.py
@@ -1,3 +1,6 @@
 RESPONSES_FILE = "responses"
+"""Name of the file which contains all custom `Responses` for Chatsky to use."""
 CONDITIONS_FILE = "conditions"
+"""Name of the file which contains all custom `Conditions` for Chatsky to use."""
 CUSTOM_FILE = "custom"
+"""Name of the directory which contains the files with custom `Conditions` and `Responses` for Chatsky to use."""
--- backend/chatsky_ui/services/json_converter/logic_component_converter/service_replacer.py
+++ backend/chatsky_ui/services/json_converter/logic_component_converter/service_replacer.py
@@ -7,12 +7,23 @@ from chatsky_ui.core.logger_config import get_logger
 
 
 class ServiceReplacer(NodeTransformer):
+    """Replaces services' old codes in a given AST. For example, when a user changes a custom condition's
+    code in the UI, the condition's code that is stored in the CUSTOM_FILE.CONDITIONS_FILE becomes outdated and
+    needs to be updated. This class can do that.
+    """
     def __init__(self, new_services: List[str]):
+        """Creates an object of `ServiceReplacer` class, parses received service codes and saves them.
+        If the code of a service isn't a class definition, logger gets an error and that service is ignored.
+
+        Args:
+            new_services (List[str]): The codes of services to replace their old codes.
+        """
         self.new_services_classes = self._get_classes_def(new_services)
         self._logger = None
 
     @property
     def logger(self):
+        """Returns this ServiceReplacer's `logger`. Raises ValueError if it isn't set."""
         if self._logger is None:
             raise ValueError("Logger has not been configured. Call set_logger() first.")
         return self._logger
@@ -21,12 +32,28 @@ class ServiceReplacer(NodeTransformer):
         self._logger = get_logger(__name__)
 
     def _get_classes_def(self, services_code: List[str]) -> Dict[str, ast.ClassDef]:
+        """Parses the services codes with 'ast.parse' and extracts all class definitions from the results.
+
+        Args:
+            services_code (List[str]): The codes of services to replace their old versions.
+        Returns:
+            Dict[str, ast.ClassDef]: A dictionary with all found classes' names as keys and respective codes as values.
+        """
         parsed_codes = [ast.parse(service_code) for service_code in services_code]
         for idx, parsed_code in enumerate(parsed_codes):
             classes = self._extract_class_defs(parsed_code, services_code[idx])
         return classes
 
     def _extract_class_defs(self, parsed_code: ast.Module, service_code: str):
+        """Returns all class definitions in a given `ast.Module`.
+
+        Args:
+            service_code (str): The code of the service being processed.
+            parsed_code (ast.Module): Same, but it's parsed through `ast.parse` first. If there are nodes which aren't
+                'ast.ClassDef', logger gets an error and the node is ignored.
+        Returns:
+            Dict[str, ast.ClassDef]: A dictionary with all found classes' names as keys and respective codes as values.
+        """
         classes = {}
         for node in parsed_code.body:
             if isinstance(node, ast.ClassDef):
@@ -36,30 +63,49 @@ class ServiceReplacer(NodeTransformer):
         return classes
 
     def visit_ClassDef(self, node: ast.ClassDef) -> ast.ClassDef:
+        """Checks if this ast.ClassDef node is supposed to be updated and updates it. Otherwise, it returns
+        the node unchanged. Uses a function which deletes the service from the list of services to be updated.
+        """
         self.logger.debug("Visiting class '%s' and comparing with: %s", node.name, self.new_services_classes.keys())
         if node.name in self.new_services_classes:
             return self._get_class_def(node)
         return node
 
     def _get_class_def(self, node: ast.ClassDef) -> ast.ClassDef:
+        """Returns the code of the service with the same name as this node.
+        Also deletes the service from the list of services to be updated.
+        """
         service = self.new_services_classes[node.name]
         del self.new_services_classes[node.name]
         self.logger.info("Updating class '%s'", node.name)
         return service
 
     def generic_visit(self, node: ast.AST):
+        """Gets called when a node within the AST isn't a ClassDef.
+        Calls the normal generic_visit(), which calls ast.visit() on all child nodes of this node.
+        Then, in case this node is an 'ast.Module', all services to be updated get appended to it.
+        """
         super().generic_visit(node)
         if isinstance(node, ast.Module) and self.new_services_classes:
             self._append_new_services(node)
         return node
 
     def _append_new_services(self, node: ast.Module):
+        """Appends all services in need of an update (parsed nodes) to the given ast.Module node."""
         self.logger.info("Services not found, appending new services: %s", list(self.new_services_classes.keys()))
         for _, service in self.new_services_classes.items():
             node.body.append(service)
 
 
 def store_custom_service(services_path: Path, services: List[str]):
+    """Stores custom services, such as conditions or responses, into a given file.
+    If the file isn't empty, it appends the new services to existing ones.
+    It stores the data as an Abstract Syntax Tree.
+
+    Args:
+        services_path (Path): path to the file where the services should be stored.
+        services (List[str]): A list of strings, each being the code of one of the services to store.
+    """
     with open(services_path, "r", encoding="UTF-8") as file:
         conditions_tree = ast.parse(file.read())
 
@@ -72,6 +118,13 @@ def store_custom_service(services_path: Path, services: List[str]):
 
 
 def get_all_classes(services_path):
+    """Retrieves the code for all services stored in a given file.
+
+    Args:
+        services_path (Path): path to the file where the services are stored.
+    Returns:
+        A list of dictionaries with fields: "name" and "body" containing the names and the codes of all stored services.
+    """
     with open(services_path, "r", encoding="UTF-8") as file:
         conditions_tree = ast.parse(file.read())
 
