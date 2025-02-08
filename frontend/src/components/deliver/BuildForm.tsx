6b7550ee9c8aca3389ec359b29272a7a24fa0443
Artem Marin <artelis57@gmail.com>
2025-02-08 12:50:12 +0300

    refactor: refactor buildForm component

--- frontend/src/components/deliver/BuildForm.tsx
+++ frontend/src/components/deliver/BuildForm.tsx
@@ -1,6 +1,7 @@
 import { messengerType } from "@/api/bot"
 import { buildContext } from "@/contexts/buildContext"
 import { Button, Divider, Input, Select, SelectItem } from "@nextui-org/react"
+import { QuestionMarkIcon } from "@radix-ui/react-icons"
 import { useContext, useEffect, useState } from "react"
 
 interface IFormData {
@@ -30,96 +31,117 @@ const BuildForm = () => {
       ...formData,
       name: formData.name || `Build ${builds.length}`,
     })
-    setFormData({
-      ...initialData,
-      name: `Build ${builds.length + 1}`,
-    })
   }
 
   useEffect(() => {
-    setFormData((prev) => ({ ...prev, name: `Run ${builds.length + 1}` }))
-  }, [builds.length])
+    setFormData((prev) => ({ ...prev, name: `Build ${builds.length}` }))
+  }, [builds])
 
   return (
     <div className='h-full w-full flex flex-col gap-3'>
-      <div className='flex-grow flex'>
-        <div className='flex flex-col min-w-[100px]'>
-          <div className='h-12 flex items-center mr-4'>
-            <span className='text-sm font-semibold text-neutral-500'>Name</span>
+      <div className='flex-grow flex flex-col'>
+        {/* NAME FIELD */}
+        <div className='flex relative'>
+          <div className='flex flex-col min-w-[100px]'>
+            <div className='h-12 flex items-center mr-4'>
+              <span className='text-sm font-semibold text-neutral-500'>Name</span>
+            </div>
           </div>
-          <Divider />
-          <div className='h-12 flex items-center mr-4'>
-            <span className='text-sm font-semibold text-neutral-500'>Messenger</span>
+          <div className='flex flex-col basis-full w-0'>
+            <div className='h-12 flex items-center justify-end'>
+              <Input
+                placeholder={`Build ${builds.length}`}
+                value={formData.name}
+                onChange={(e) => {
+                  setFormData((prev) => ({
+                    ...prev,
+                    name: e.target.value,
+                  }))
+                }}
+                disableAnimation
+                size='sm'
+                variant='underlined'
+                classNames={{
+                  inputWrapper: ["border-none", "data-[focus=true]:after:h-0", "shadow-none"],
+                  input: ["w-full", "truncate", "placeholder:text-input-border"],
+                }}
+              />
+            </div>
           </div>
-          <Divider />
-          <div className='h-12 flex items-center mr-4'>
-            <span className='text-sm font-semibold text-neutral-500'>Preset</span>
-          </div>
+          <Divider className='absolute bottom-0' />
         </div>
 
-        <div className='flex flex-col basis-full w-0'>
-          <div className='h-12 flex items-center justify-end'>
-            <Input
-              placeholder={`Build ${builds.length}`}
-              value={formData.name}
-              onChange={(e) => {
-                setFormData((prev) => ({
-                  ...prev,
-                  name: e.target.value,
-                }))
-              }}
-              disableAnimation
-              size='sm'
-              variant='underlined'
-            />
+        {/* MESSENGER FIELD */}
+        <div className='flex relative'>
+          <div className='flex flex-col min-w-[100px]'>
+            <div className='h-12 flex items-center mr-4'>
+              <span className='text-sm font-semibold text-neutral-500'>Messenger</span>
+            </div>
           </div>
-          <Divider />
-          <div className='h-12 flex items-center justify-end'>
-            <Select
-              aria-label='Messenger'
-              labelPlacement='outside'
-              placeholder='Web'
-              defaultSelectedKeys={["web"]}
-              value={formData.messenger}
-              onChange={(e) => {
-                setFormData(
-                  (prev) =>
-                    ({
-                      ...prev,
-                      messenger: e.target.value || "web",
-                    } as IFormData)
-                )
-              }}
-              radius='sm'
-              size='sm'
-            >
-              {messengers.map((item) => (
-                <SelectItem key={item.key}>{item.label}</SelectItem>
-              ))}
-            </Select>
+          <div className='flex flex-col basis-full w-0'>
+            <div className='h-12 flex items-center justify-end'>
+              <Select
+                aria-label='Messenger'
+                labelPlacement='outside'
+                placeholder='Web'
+                defaultSelectedKeys={["web"]}
+                value={formData.messenger}
+                onChange={(e) => {
+                  setFormData(
+                    (prev) =>
+                      ({
+                        ...prev,
+                        messenger: e.target.value || "web",
+                      } as IFormData)
+                  )
+                }}
+                radius='sm'
+                size='sm'
+              >
+                {messengers.map((item) => (
+                  <SelectItem key={item.key}>{item.label}</SelectItem>
+                ))}
+              </Select>
+            </div>
           </div>
-          <Divider />
-          <div className='h-12 flex items-center justify-end'>
-            <Select
-              aria-label='Messenger'
-              placeholder='No preset'
-              labelPlacement='outside'
-              defaultSelectedKeys={["None"]}
-              value={formData.preset}
-              onChange={(e) => {
-                setFormData((prev) => ({ ...prev, preset: e.target.value || "None" }))
-              }}
-              radius='sm'
-              size='sm'
-            >
-              {[{ key: "None", label: "No preset" }].map((item) => (
-                <SelectItem key={item.key}>{item.label}</SelectItem>
-              ))}
-            </Select>
+          <Divider className='absolute bottom-0' />
+        </div>
+
+        {/* PRESET FIELD */}
+        <div className='flex relative'>
+          <div className='flex flex-col min-w-[100px]'>
+            <div className='h-12 flex items-center mr-4'>
+              <span className='text-sm font-semibold text-neutral-500'>Preset</span>
+            </div>
           </div>
+          <div className='flex flex-col basis-full w-0'>
+            <div className='h-12 flex items-center justify-end'>
+              <Select
+                aria-label='Preset'
+                placeholder='No preset'
+                labelPlacement='outside'
+                defaultSelectedKeys={["None"]}
+                value={formData.preset}
+                onChange={(e) => {
+                  setFormData((prev) => ({ ...prev, preset: e.target.value || "None" }))
+                }}
+                radius='sm'
+                size='sm'
+              >
+                {[{ key: "None", label: "No preset" }].map((item) => (
+                  <SelectItem key={item.key}>{item.label}</SelectItem>
+                ))}
+              </Select>
+            </div>
+          </div>
+          <Divider className='absolute bottom-0' />
         </div>
       </div>
-      <div>
+
+      <div className='flex gap-3'>
+        <Button isIconOnly className='rounded-full'>
+          <QuestionMarkIcon className='w-5 h-5' />
+        </Button>
         <Button
           onClick={buildHandler}
           disabled={buildPending}
