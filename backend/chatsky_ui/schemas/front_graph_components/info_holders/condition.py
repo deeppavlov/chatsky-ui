f16d98b866812ca836df2aa1576972e0baae41db
Ramimashkouk <ramima1996@gmail.com>
2025-02-14 18:41:56 +0800

    feat: Support basic conditions

--- backend/chatsky_ui/schemas/front_graph_components/info_holders/condition.py
+++ backend/chatsky_ui/schemas/front_graph_components/info_holders/condition.py
@@ -1,3 +1,6 @@
+from typing import List, Optional, Dict, Union
+from pydantic import Field, model_validator
+
 from ..base_component import BaseComponent
 from chatsky.conditions import All, Any, ExactMatch, Regexp, HasText, Not
 
@@ -11,3 +14,40 @@ class CustomCondition(Condition):
 
 class SlotCondition(Condition):
     slot_id: str  # not the condition id
+
+
+class ExactMatchCondition(ExactMatch):
+    match: str
+
+
+class IncludeTextCondition(HasText):
+    text: str
+
+
+class NotCondition(Not):
+    condition: dict
+
+
+class RegexpCondition(Regexp):
+    pattern: str
+    flags: Dict[str, str] = Field(default_factory=dict, description="Flags for the regex pattern")
+
+    @model_validator(mode="after")
+    def validate_flags(self):
+        allowed_keys = {'ignoreCase'}
+        for key in self.flags.keys():
+            if key not in allowed_keys:
+                raise ValueError(f"Invalid key '{key}' in flags. Allowed keys are {allowed_keys}")
+        return self
+
+
+class AllOfCondition(BaseComponent):
+    conditions: List[dict]
+
+
+class AnyOfCondition(BaseComponent):
+    conditions: List[dict]
+
+
+class ChatskyCondition(Condition):
+    condition: Union[ExactMatchCondition, IncludeTextCondition, NotCondition, RegexpCondition, AllOfCondition, AnyOfCondition]
--- backend/chatsky_ui/services/json_converter/logic_component_converter/condition_converter.py
+++ backend/chatsky_ui/services/json_converter/logic_component_converter/condition_converter.py
@@ -55,35 +55,3 @@ class SlotConditionConverter(ConditionConverter):
     def get_pre_transitions(self):
         slot_path = self.slots_conf[self.condition.slot_id]  # type: ignore
         return {slot_path: {"chatsky.processing.slots.Extract": slot_path}}
-
-
-class ChatskyConditionConverter(ConditionConverter):
-    def __init__(self, condition: dict):
-        if condition["type"] != "basic":
-            raise BadConditionException("Unsupported condition type '%s'" % condition["type"])
-
-        structure = condition["data"]["structure"]
-        cnd_details = condition["data"][structure]
-        if structure in ["All", "Any"]:
-            cnd_details = [ChatskyConditionConverter(cnd).condition for cnd in cnd_details]
-            print(f"cnd_details {structure} condition to {cnd_details}")
-        elif structure == "Not":
-            cnd_details = ChatskyConditionConverter(cnd_details).condition
-            print(f"Converted Not condition to {cnd_details}")
-
-        if isinstance(cnd_details, dict):
-            self.condition = globals()[structure](**cnd_details) 
-            print(f"Converted {structure} condition to {cnd_details}")
-        elif isinstance(cnd_details, list):
-            self.condition = globals()[structure](*cnd_details)
-            print(f"Converted {structure} condition to {cnd_details}")
-        else:
-            self.condition = globals()[structure](cnd_details)
-            print(f"Converted {structure} condition to {cnd_details}")
-        
-
-    def _convert(self):
-        return {f"chatsky.conditions.{self.condition.__class__.__name__}": self.condition.model_dump()}
-
-    def get_pre_transitions(self):
-        return {}
