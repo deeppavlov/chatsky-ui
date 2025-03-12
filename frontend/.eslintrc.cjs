d0c83624ea8d40e565cfd740d13177e2b12ff330
Artem Marin <artelis57@gmail.com>
2025-03-12 15:06:21 +0300

    chore: add ESLint plugin for Prettier integration

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,15 +5,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-unused-vars": "warn",
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 }
