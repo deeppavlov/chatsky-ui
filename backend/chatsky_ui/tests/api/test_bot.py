168f6773c07d57b0866406f7dc06ee23685c42aa
ZergLev <vasilev.sv@phystech.edu>
2025-03-31 12:42:20 +0300

    fix: test updated up to refactored code

--- backend/chatsky_ui/tests/api/test_bot.py
+++ backend/chatsky_ui/tests/api/test_bot.py
@@ -220,9 +220,7 @@ async def test_get_chat_records(dummy_run_id):
 
 @pytest.mark.asyncio
 async def test_get_chat_ids():
-    test_result = [
-        ["0_0"],
-    ]
+    test_result = ["0_0"]
 
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as async_client:
         get_response = await async_client.get("/api/v1/bot/get_chat_ids")
