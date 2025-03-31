b9e7a568f7d37518f42037da4548f79508d1af5a
ZergLev <vasilev.sv@phystech.edu>
2025-03-31 11:09:13 +0300

    style: removed unnecessary f-string

--- backend/chatsky_ui/tests/api/test_bot.py
+++ backend/chatsky_ui/tests/api/test_bot.py
@@ -225,7 +225,7 @@ async def test_get_chat_ids():
     ]
 
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as async_client:
-        get_response = await async_client.get(f"/api/v1/bot/get_chat_ids")
+        get_response = await async_client.get("/api/v1/bot/get_chat_ids")
 
         assert get_response.status_code == 200
         assert test_result == get_response.json()
