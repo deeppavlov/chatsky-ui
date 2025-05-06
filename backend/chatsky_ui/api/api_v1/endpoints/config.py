488da33e5f53969fa236e92c8b3dbe27551ebe91
Rami Mashkouk <rami.n.mashkouk@gmail.com>
2025-05-06 17:23:42 +0330

    feat: Provide id for LLMs tokens & configs

--- backend/chatsky_ui/api/api_v1/endpoints/config.py
+++ backend/chatsky_ui/api/api_v1/endpoints/config.py
@@ -1,20 +1,63 @@
-from fastapi import APIRouter, HTTPException, status
-from omegaconf import OmegaConf
+import uuid
 from typing import Optional
 
-from dotenv import load_dotenv
+from fastapi import APIRouter, HTTPException, status
+from omegaconf import OmegaConf
+
 from chatsky_ui import __version__
 from chatsky_ui.core.config import settings
-from chatsky_ui.schemas.front_graph_components.llm_model import LLMModel
+from chatsky_ui.core.logger_config import get_logger
 from chatsky_ui.db.base import read_conf, write_conf
-from chatsky_ui.core.logger_config import get_logger  # noqa: E402
+from chatsky_ui.schemas.front_graph_components.llm_model import LLMModel
 
 router = APIRouter()
 
 
-def _form_token_name(token_name: str, provider: str) -> str:
-    """Forms the token name for the given provider and token name."""
-    return "_".join(["llm", token_name, provider])
+async def _save_token_data(
+    token_id: str, token_name: Optional[str] = None, provider: Optional[str] = None, add_if_not_exists: bool = True
+):
+    """Saves the token data to llms yaml file."""
+    logger = get_logger(__name__)
+
+    omega_llms_conf = await read_conf(settings.llms_conf_path, settings.llms_path_lock)
+    llms_conf = OmegaConf.to_container(omega_llms_conf, resolve=True)
+
+    if "tokens" not in llms_conf:
+        llms_conf["tokens"] = {}
+
+    for id, token_conf in llms_conf["tokens"].items():
+        if id == token_id:
+            old_token_name = token_conf["name"]
+            if token_name is not None:
+                token_conf["name"] = token_name
+            if provider is not None:
+                token_conf["provider"] = provider
+            try:
+                settings.remove_env_vars([old_token_name])
+            except ValueError:
+                logger.warning(
+                    f"Unexpexted behavior: Token '{old_token_name}' for provider '{token_conf['provider']}' not found "
+                    f"in `.env`. Please check your environment variables."
+                )
+            break
+    else:
+        if not add_if_not_exists:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Token ID '{token_id}' not found. Use POST to create it.",
+            )
+        llms_conf["tokens"].update(
+            {
+                token_id: {
+                    "name": token_name,
+                    "provider": provider,
+                }
+            }
+        )
+
+    await write_conf(llms_conf, settings.llms_conf_path, settings.llms_path_lock)
+
+    return llms_conf
 
 
 @router.get("/version")
@@ -29,96 +72,122 @@ async def get_providers():
     return LLMModel.PROVIDERS
 
 
+# change to return the id along with the name and the provider
 @router.get("/llms/tokens")
 async def get_llm_tokens():
-    env_vars = settings.get_env_vars("llm")
-    tokens = []
-    for key, _ in env_vars.items():
-        provider = key.split("_")[-1]
-        key = "_".join(key.split("_")[1:-1])
-        tokens.append((key, provider))
-        
-    return tokens
+    omega_llms_conf = await read_conf(settings.llms_conf_path, settings.llms_path_lock)
+    llms_conf = OmegaConf.to_container(omega_llms_conf, resolve=True)
+
+    tokens = llms_conf.get("tokens", {})
+    return {"status": "ok", "data": tokens}
 
 
 @router.post("/llms/token")
 async def post_llm_token(provider: str, token_name: str, token_value: str):
     """Creates a new token for an LLM provider."""
+    token_name = token_name.strip().replace(" ", "_")
     if provider not in LLMModel.PROVIDERS:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Provider '{provider}' not found. Available providers: {', '.join(LLMModel.PROVIDERS.keys())}",
         )
-    
-    env_vars = settings.get_env_vars(_form_token_name(token_name, provider))
+
+    env_vars = settings.get_env_vars(token_name)
     if env_vars:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Token '{token_name}' already exists. Use PATCH to update it.",
+            detail=f"Token '{token_name}' already exists. Please use another name.",
         )
 
-    settings.add_env_vars({_form_token_name(token_name, provider): token_value})
-    return {"status": "ok", "message": "Token saved successfully"}
+    token_id = str(uuid.uuid4())[:8]
+    await _save_token_data(token_id, token_name, provider)
+    settings.add_env_vars({token_name: token_value})
+    return {"status": "ok", "token_id": token_id, "message": "Token saved successfully"}
 
 
 @router.patch("/llms/token")
-async def patch_llm_token(provider: str, old_token_name:str, new_token_name: Optional[str] = None, new_token_value: Optional[str] = None):
+async def patch_llm_token(
+    token_id: str,
+    new_token_name: Optional[str] = None,
+    new_provider: Optional[str] = None,
+    new_token_value: Optional[str] = None,
+):
     """Updates an existing token for an LLM provider."""
-    if provider not in LLMModel.PROVIDERS:
+    if not any([new_provider, new_token_value, new_token_name]):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="At least one of 'provider', 'token_name', or 'token_value' must be provided.",
+        )
+
+    if new_provider is not None and new_provider not in LLMModel.PROVIDERS:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Provider '{provider}' not found. Available providers: {', '.join(LLMModel.PROVIDERS.keys())}",
+            detail=f"Provider '{new_provider}' not found. Available providers: {', '.join(LLMModel.PROVIDERS.keys())}",
         )
 
     if new_token_name is not None:
-        env_vars = settings.get_env_vars(_form_token_name(new_token_name, provider))
+        env_vars = settings.get_env_vars(new_token_name)
         if env_vars:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Token '{new_token_name}' already exists. Use PATCH to update it.",
+                detail=f"Token '{new_token_name}' already exists. Choose another name.",
             )
 
-    new_token_name = new_token_name or old_token_name
-    new_token_value = new_token_value or settings.get_env_vars(_form_token_name(old_token_name, provider)).get(_form_token_name(old_token_name, provider))
-
-    try:
-        settings.remove_env_vars([_form_token_name(old_token_name, provider)])
-    except ValueError:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Token '{old_token_name}' for provider '{provider}' not found.",
-        )
-    settings.add_env_vars({_form_token_name(new_token_name, provider): new_token_value})
+    conf = await _save_token_data(token_id, new_token_name, new_provider, add_if_not_exists=False)
+    if new_token_value is not None:
+        new_token_name = new_token_name or [token["name"] for token in conf["tokens"]][0]
+        settings.add_env_vars({new_token_name: new_token_value})
 
     return {"status": "ok", "message": "Token updated successfully"}
 
 
 @router.delete("/llms/token")
-async def delete_llm_token(provider: str, token_name: str):
+async def delete_llm_token(id: str):
+    omega_llms_conf = await read_conf(settings.llms_conf_path, settings.llms_path_lock)
+    llms_conf = OmegaConf.to_container(omega_llms_conf, resolve=True)
+
+    tokens = llms_conf.get("tokens", {})
+
+    if id not in tokens:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Token with ID '{id}' not found.",
+        )
+    token_name = tokens[id]["name"]
+    provider = tokens[id]["provider"]
+
+    del tokens[id]
+    await write_conf(llms_conf, settings.llms_conf_path, settings.llms_path_lock)
+
     try:
-        settings.remove_env_vars([_form_token_name(token_name, provider)])
+        settings.remove_env_vars([token_name])
     except ValueError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Token '{token_name}' for provider '{provider}' not found.",
         )
+
     return {"status": "ok", "message": "Token deleted successfully"}
+
 
 @router.get("/llms")
 async def get_llm_models():
     """Returns the list of LLM models."""
     omega_llms_conf = await read_conf(settings.llms_conf_path, settings.llms_path_lock)
     llms_conf = OmegaConf.to_container(omega_llms_conf, resolve=True)
-    return {"status": "ok", "data": llms_conf}
+    return {"status": "ok", "data": llms_conf.get("config_models", {})}
 
 
 @router.post("/llms")
-async def post_llm_model(config_name: str, model_name: str, llm_token_name: str, system_prompt: str = ""):
+async def post_llm_model(config_name: str, model_name: str, llm_token_id: str, system_prompt: str = ""):
     """Creates a new LLM model configuration."""
     omega_llms_conf = await read_conf(settings.llms_conf_path, settings.llms_path_lock)
     llms_conf = OmegaConf.to_container(omega_llms_conf, resolve=True)
 
-    if config_name in llms_conf:
+    if "config_models" not in llms_conf:
+        llms_conf["config_models"] = {}
+
+    if config_name in llms_conf["config_models"]:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"LLM model '{config_name}' already exists. Use PATCH to update it.",
@@ -130,40 +199,52 @@ async def post_llm_model(config_name: str, model_name: str, llm_token_name: str,
             detail=f"Model '{model_name}' not found. Available models: {', '.join(LLMModel.MODELS)}",
         )
 
-    env_vars = settings.get_env_vars(f"llm_{llm_token_name}")
-    llm_token_value = next(iter(env_vars.values()), None)
-    if llm_token_value is None or llm_token_value == "":
+    if llm_token_id not in llms_conf.get("tokens", {}):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Token '{llm_token_name}' doesn't exist. Please set it in the environment variables.",
+            detail=f"Token ID '{llm_token_id}' not found. Please create it first.",
         )
 
-    llms_conf[config_name] = {
-        "model_name": model_name,
-        "token_name": llm_token_name,
-        "system_prompt": system_prompt,
-    }
+    config_model_id = str(uuid.uuid4())[:8]
+    llms_conf["config_models"].update(
+        {
+            config_model_id: {
+                "config_name": config_name,
+                "model_name": model_name,
+                "token_id": llm_token_id,
+                "system_prompt": system_prompt,
+            }
+        }
+    )
 
     await write_conf(llms_conf, settings.llms_conf_path, settings.llms_path_lock)
-    return {"status": "ok", "message": "LLM model created successfully"}
+    return {"status": "ok", "token_id": config_model_id, "message": "LLM model created successfully"}
 
 
 @router.patch("/llms")
 async def patch_llm_model(
-    old_config_name: str,
+    config_id: str,
     new_config_name: str = None,
     model_name: str = None,
-    llm_token_name: str = None,
-    system_prompt: str = None
+    llm_token_id: str = None,
+    system_prompt: str = None,
 ):
     """Updates an existing LLM model configuration."""
+    if not any([new_config_name, model_name, llm_token_id, system_prompt]):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="At least one of new_config_name, model_name, llm_token_id, or system_prompt must be provided.",
+        )
+
     omega_llms_conf = await read_conf(settings.llms_conf_path, settings.llms_path_lock)
     llms_conf = OmegaConf.to_container(omega_llms_conf, resolve=True)
 
-    if old_config_name not in llms_conf:
+    config_models = llms_conf["config_models"]
+
+    if config_id not in config_models:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"LLM model '{old_config_name}' not found. Use POST to create it.",
+            detail=f"LLM model '{config_id}' not found. Use POST to create it.",
         )
 
     if model_name and model_name not in LLMModel.MODELS:
@@ -171,49 +252,46 @@ async def patch_llm_model(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Model '{model_name}' not found. Available models: {', '.join(LLMModel.MODELS)}",
         )
-    
-    if new_config_name:
-        if new_config_name in llms_conf:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"LLM model '{new_config_name}' already exists. Use PATCH to update it.",
-            )
-        llms_conf[new_config_name] = llms_conf.pop(old_config_name)
-        config_name = new_config_name
-    else:
-        config_name = old_config_name
 
-    if llm_token_name:
-        env_vars = settings.get_env_vars(f"llm_{llm_token_name}")
-        llm_token_value = next(iter(env_vars.values()), None)
-        if llm_token_value is None or llm_token_value == "":
+    if new_config_name and new_config_name in config_models:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"LLM model '{new_config_name}' already exists. Use PATCH to update it.",
+        )
+
+    if llm_token_id:
+        if llm_token_id not in llms_conf.get("tokens", {}):
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Token '{llm_token_name}' doesn't exist. Please set it in the environment variables.",
+                detail=f"Token '{llm_token_id}' doesn't exist. Please set it in the environment variables.",
             )
-        llms_conf[config_name]["token_name"] = llm_token_name
 
-    if model_name:
-        llms_conf[config_name]["model_name"] = model_name
-
-    if system_prompt is not None:
-        llms_conf[config_name]["system_prompt"] = system_prompt
+    llms_conf["config_models"].update(
+        {
+            config_id: {
+                "config_name": new_config_name if new_config_name else config_models[config_id]["config_name"],
+                "model_name": model_name if model_name else config_models[config_id]["model_name"],
+                "token_id": llm_token_id if llm_token_id else config_models[config_id]["token_id"],
+                "system_prompt": system_prompt if system_prompt else config_models[config_id]["system_prompt"],
+            }
+        }
+    )
 
     await write_conf(llms_conf, settings.llms_conf_path, settings.llms_path_lock)
     return {"status": "ok", "message": "LLM model updated successfully"}
 
 
 @router.delete("/llms")
-async def delete_llm_model(config_name: str):
+async def delete_llm_model(config_id: str):
     omega_llms_conf = await read_conf(settings.llms_conf_path, settings.llms_path_lock)
     llms_conf = OmegaConf.to_container(omega_llms_conf, resolve=True)
 
-    if config_name not in llms_conf:
+    if config_id not in llms_conf["config_models"]:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"LLM model '{config_name}' not found.",
+            detail=f"LLM model '{config_id}' not found.",
         )
 
-    del llms_conf[config_name]
+    del llms_conf["config_models"][config_id]
     await write_conf(llms_conf, settings.llms_conf_path, settings.llms_path_lock)
     return {"status": "ok", "message": "LLM model deleted successfully"}
--- backend/chatsky_ui/services/json_converter/pipeline_converter.py
+++ backend/chatsky_ui/services/json_converter/pipeline_converter.py
@@ -1,9 +1,9 @@
 import asyncio
 from pathlib import Path
 from typing import Optional
-from omegaconf import OmegaConf
 
 import yaml
+from omegaconf import OmegaConf
 
 try:
     from yaml import CDumper as Dumper
@@ -11,8 +11,8 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-from ...db.base import read_conf
 from ...core.config import settings
+from ...db.base import read_conf
 from ...schemas.front_graph_components.pipeline import Pipeline
 from .base_converter import BaseConverter
 from .consts import UNIQUE_BUILD_TOKEN
@@ -42,11 +42,8 @@ class PipelineConverter(BaseConverter):
         self.from_yaml(file_path=input_file)
 
         loop = asyncio.get_event_loop()
-        llm_configurations_omega = loop.run_until_complete(
-            read_conf(settings.llms_conf_path, settings.llms_path_lock)
-        )
+        llm_configurations_omega = loop.run_until_complete(read_conf(settings.llms_conf_path, settings.llms_path_lock))
         llm_configurations = OmegaConf.to_container(llm_configurations_omega, resolve=True)
-
 
         self.pipeline = Pipeline(
             messenger={
@@ -94,7 +91,9 @@ class PipelineConverter(BaseConverter):
 
         return {
             "script": script_converter(slots_conf=slots_conf),
-            "models": LLMModelsConverter(self.pipeline.llm_configurations)() if self.pipeline.llm_configurations else {},
+            "models": LLMModelsConverter(self.pipeline.llm_configurations)()
+            if self.pipeline.llm_configurations
+            else {},
             "messenger_interface": MessengerConverter(self.pipeline.messenger)(),
             "slots": slots_converter(),
             "start_label": start_label,
