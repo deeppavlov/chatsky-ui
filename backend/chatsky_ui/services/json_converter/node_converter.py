f6c98d36f67306b5a8b7dbd2dbb04d04c1008f05
ZergLev <vasilev.sv@phystech.edu>
2025-05-12 12:55:48 +0300

    refactor: hideButtons renamed to showButtons + changed the expected buttonsData structure from frontend

--- backend/chatsky_ui/services/json_converter/node_converter.py
+++ backend/chatsky_ui/services/json_converter/node_converter.py
@@ -103,8 +103,9 @@ class InfoNodeConverter(NodeConverter):
             PRE_RESPONSE: {"fill": {"chatsky.processing.FillTemplate": None}},
         }
 
-        remove_buttons = self.node.removeButtons
-        if remove_buttons is True:
+        buttons = self.node.buttonsData
+        show_buttons = buttons.get("showButtons", False)
+        if show_buttons is False:
             result[PRE_RESPONSE].update(
                 {
                     "1_remove_telegram_reply_keyboard": {
@@ -112,7 +113,7 @@ class InfoNodeConverter(NodeConverter):
                     }
                 }
             )
-        buttons = self.node.buttonsData
+
         if buttons is not None:
             result[PRE_RESPONSE].update({"2_add_buttons": ButtonsConverter(buttons)()})
 
